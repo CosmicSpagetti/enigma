bce900a0740389a22ed7c27b98a43a3d1681206a
billy urrutia <billyurrutia@gmail.com>
2019-04-13 12:15:27 -0600

    Created offset initialize with a default of todays date and created test for creating offset

---
+++ lib/offset.rb
@@ -0,0 +1,7 @@
+class Offset
+  attr_reader :date
+  def initialize(date = Time.now.strftime("%d%m%y"))
+    @date = date
+  end
+
+end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -5,6 +5,29 @@ class OffsetTest < Minitest::Test
   def test_it_exists
     offset = Offset.new("040895")
 
-    assert_instance_of Offset, offset 
+    assert_instance_of Offset, offset
+  end
+
+  def test_it_has_date
+    offset = Offset.new("040895")
+
+    assert_equal "040895", offset.date
+  end
+
+  def test_can_get_todays_date_with_no_input
+    offset = Offset.new
+
+    assert_equal "130419", offset.date
+  end
+
+  def test_can_create_offset_from_date
+    offset = Offset.new("040895")
+    expected = {
+      a_offset: 1,
+      b_offset: 0,
+      c_offset: 2,
+      d_offset: 5
+    }
+    assert_equal expected, offset.create_offset 
   end
 end
