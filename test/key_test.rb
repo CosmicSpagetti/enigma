4fbca2a8bab832f21e0f395b59f19353b52ae694
billy urrutia <billyurrutia@gmail.com>
2019-04-12 21:59:40 -0600

    Made a split key test and method that puts keys in hashes

--- test/key_test.rb
+++ test/key_test.rb
@@ -15,8 +15,20 @@ class KeyTest < Minitest::Test
 
   def test_can_get_5_digit_key_random_key
     key = Key.new
-
+    binding.pry
     assert_equal 5, key.key.length
+  end
+
+  def test_can_create_keys
+    key = Key.new("02715")
+    expected = {
+      a_key: 02,
+      b_key: 27,
+      c_key: 71,
+      d_key: 15
+    }
+
+    assert_equal expected, key.split_key
   end
 
 
