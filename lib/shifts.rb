af0a24d2d8da76b0bc1db8ecdfa07857dddfe691
billy urrutia <billyurrutia@gmail.com>
2019-04-14 13:52:57 -0600

    Created Shifter method that correctly encrypts message with test not sure how this will all work out

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -1,9 +1,12 @@
 class Shifts
   attr_reader :key,
-              :date
+              :date,
+              :alphabet
   def initialize(key = Key.new, date = Offset.new)
     @key = key.class == String ? Key.new(key) : key
     @date = date.class == String ? Offset.new(date) : date
+    @alphabet = ("a".."z").to_a << " "
+    @final_values = final_shifts
   end
 
   def final_shifts
@@ -14,5 +17,23 @@ class Shifts
     final_hash[:D] = @key.key_hash[:d_key] + @date.offset[:d_offset]
     final_hash
   end
+
+  def shifter(message)
+    message.downcase.chars.map.with_index do |char, index|
+      next char unless @alphabet.include?(char)
+      if index == 0 || index % 4 == 0
+        char.tr(@alphabet.to_s, @alphabet.rotate(@final_values[:A]).to_s)
+      elsif index % 4 == 1
+        char.tr(@alphabet.to_s, @alphabet.rotate(@final_values[:B]).to_s)
+      elsif index % 4 == 2
+        char.tr(@alphabet.to_s, @alphabet.rotate(@final_values[:C]).to_s)
+      elsif index % 4 == 3
+        char.tr(@alphabet.to_s, @alphabet.rotate(@final_values[:D]).to_s)
+      end
+    end.join
+
+  end
+
+
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,6 +7,7 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
+
   def test_can_encrypt_message
     enigma = Enigma.new
     expected = {
@@ -19,11 +20,3 @@ class EnigmaTest < Minitest::Test
   end
 
 end
-
-# The encrypt method takes a message String as an argument. It can optionally take a Key and Date as arguments to use for encryption. If the key is not included, generate a random key. If the date is not included, use today’s date.
-#
-# The encrypt method returns a hash with three keys:
-#
-# :encryption => the encrypted String
-# :key => the key used for encryption as a String
-# :date => the date used for encryption as a String in the form DDMMYY
