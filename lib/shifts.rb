c7f5320a1d3d979e6ba63bb140ef7f4e2db62d34
billy urrutia <billyurrutia@gmail.com>
2019-04-14 11:37:51 -0600

    Finished final shifts hash now to actually apply it

--- lib/shifts.rb
+++ lib/shifts.rb
@@ -6,12 +6,12 @@ class Shifts
     @date = date.class == String ? Offset.new(date) : date
   end
 
-  def final_shifts(key, date)
+  def final_shifts
     final_hash = {}
-    final_hash[:A] =
-    final_hash[:B] =
-    final_hash[:C] =
-    final_hash[:D] =
+    final_hash[:A] = @key.key_hash[:a_key] + @date.offset[:a_offset]
+    final_hash[:B] = @key.key_hash[:b_key] + @date.offset[:b_offset]
+    final_hash[:C] = @key.key_hash[:c_key] + @date.offset[:c_offset]
+    final_hash[:D] = @key.key_hash[:d_key] + @date.offset[:d_offset]
     final_hash
   end
 
--- test/shifts_test.rb
+++ test/shifts_test.rb
@@ -3,7 +3,6 @@ require_relative 'test_helper'
 class ShiftsTest < Minitest::Test
 
   def test_it_exists
-    binding.pry
     shifts = Shifts.new("02715", "040895")
 
     assert_instance_of Shifts, shifts
@@ -20,19 +19,20 @@ class ShiftsTest < Minitest::Test
   def test_it_can_have_random_key_and_todays_date_with_no_input
     shifts = Shifts.new
 
-
+    assert_instance_of Key, shifts.key
+    assert_instance_of Offset, shifts.date
   end
 
 
-  # def test_can_get_final_shifts
-  #   shifts = Shifts.new
-  #   key =
-  #   expected = {
-  #     A: 3,
-  #     B: 27,
-  #     C: 73,
-  #     D: 20
-  #   }
-  #   assert_equal expected, shifts.final_shifts(key, date)
-  # end
+  def test_can_get_final_shifts
+    shifts = Shifts.new("02715", "040895")
+    key =
+    expected = {
+      A: 3,
+      B: 27,
+      C: 73,
+      D: 20
+    }
+    assert_equal expected, shifts.final_shifts
+  end
 end
