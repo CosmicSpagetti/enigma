2e301599a5966c3bc803f968d284ec2926385934
billy urrutia <billyurrutia@gmail.com>
2019-04-15 17:06:41 -0600

    Created crack.rb

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,20 +2,16 @@ class Enigma
 
   def encrypt(message, key = Key.new, date = Offset.new)
     shifts = Shifts.new(key, date)
-    encrypt_hash = {
-      encryption:shifts.shifter(message) ,
-      key: shifts.key.key ,
-      date: shifts.date.date
-    }
+    { encryption:shifts.shifter(message) ,
+      key: shifts.key_instance.key ,
+      date: shifts.date_instance.date }
   end
 
   def decrypt(message, key = Key.new, date = Offset.new)
     shifts = Shifts.new(key, date)
-    decrypt_hash = {
-      decryption:shifts.deshifter(message) ,
-      key: shifts.key.key ,
-      date: shifts.date.date
-    }
+    { decryption:shifts.deshifter(message) ,
+      key: shifts.key_instance.key ,
+      date: shifts.date_instance.date }
   end
 
   def crack(message, date = Offset.new)
@@ -26,7 +22,6 @@ class Enigma
       key += 1
       key_format = key.to_s[-5..-1]
       cracked_message = decrypt(message, key_format, date)
-      break if key_format == "99999"
     end
     decrypt(message, key_format, date)
   end
